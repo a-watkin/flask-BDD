b8d52763357d1543b72ca2e312b3427a1a70a71a
Adam <atomicpenguiens@gmail.com>
2017-04-14 18:18:44 +0100

    initial commit of flask-BDD

---
+++ features/environment.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import tempfile
+
+# add module to syspath
+cwd = os.path.abspath(os.path.dirname(__file__))
+project = os.path.basename(cwd)
+new_path = cwd.strip(project)
+# Removed 'flaskr' here from the arguments to path
+full_path = os.path.join(new_path)
+
+
+print(full_path)
+try:
+    from flaskr import app, init_db
+except ImportError:
+    sys.path.append(full_path)
+    from flaskr import app, init_db
+
+
+def before_feature(context, feature):
+    app.config['TESTING'] = True
+    context.db, app.config['DATABASE'] = tempfile.mkstemp()
+                    # provides a mock up of the app
+    context.client = app.test_client()
+    init_db()
+
+
+def after_feature(context, feature):
+    os.close(context.db)
+    os.unlink(app.config['DATABASE'])
